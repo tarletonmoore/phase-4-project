55d345797d49ee0b7ca2340d0c8922357b12b0fb
John Tarleton Moore <tarletonmoore@gmail.com>
2023-01-10 11:45:39 -0500

    set up associations

--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,2 +1,6 @@
 class BakedGood < ApplicationRecord
+    belongs_to :user
+    has_one :bakery, through: :user
+    validates :title, presence: true
+    validates :instructions, length: {minimum: 50}
 end
--- app/models/bakery.rb
+++ app/models/bakery.rb
@@ -1,2 +1,4 @@
 class Bakery < ApplicationRecord
+    has_many :users
+    has_many :baked_goods, through: :user
 end
